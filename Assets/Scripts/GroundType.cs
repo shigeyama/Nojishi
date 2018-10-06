2879d357c1a4030e0b6aa3f7690774cd63c6b7db
Shigeyama <nkc.wataru.shigeyama@gmail.com>
2018-10-06 14:03:14 +0900

    ファーム見た目追加

---
+++ Assets/Scripts/GroundType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundType : MonoBehaviour
+{
+
+    [SerializeField, Header("畑スペース")]
+    bool isFarm;
+
+    [SerializeField, Header("罠スペース")]
+    bool isTrap;
+
+    public bool IsFarm
+    {
+        get { return isFarm; }
+    }
+
+    public bool IsTrap
+    {
+        get { return isTrap; }
+    }
+}
